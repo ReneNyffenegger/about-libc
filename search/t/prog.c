671c1e71fb0d41ebe024eb2a904dfbab00f31391
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-09-08 20:10:32 +0200

    + prog.c

---
+++ search/t/prog.c
@@ -0,0 +1,56 @@
+#include <search.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void *tree_root = 0;
+
+typedef struct {
+    int         key;
+    const char* value;
+} item;
+
+int compare_item_keys(const void *item_left, const void *item_right) {
+    return ((item*)item_left)->key - ((item*)item_right)->key;
+}
+
+void insert_item(int k, const char* v) {
+
+    item *i = malloc(sizeof(item));
+    i->key   = k;
+    i->value = v;
+
+ // insert
+    tsearch(i, &tree_root, compare_item_keys);
+}
+
+void search_item(int k) {
+
+    item *result = malloc(sizeof(item));
+    result->key = 5;
+
+ // search
+    void *r = tfind(result, &tree_root, compare_item_keys);
+
+    printf("%d -> %s\n", k, (*(item**)r)->value);
+    free(result);
+}
+
+int main(int argc, char **argv) {
+    void *root = 0;
+
+    insert_item(4, "four" );
+    insert_item(7, "seven");
+    insert_item(2, "two"  );
+    insert_item(5, "five" );
+    insert_item(9, "nine" );
+    insert_item(1, "one"  );
+    insert_item(8, "eight");
+    insert_item(6, "six"  );
+
+    find_item(5);
+    find_item(4);
+    find_item(5);
+
+    return 0;
+}
