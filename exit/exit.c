82c122728346cce48e79087b1d381bb38733dfdf
René Nyffenegger <mail@renenyffenegger.ch>
2018-09-22 07:31:00 +0200

    + exit.c

---
+++ exit/exit.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main() {
+
+ //
+ // Don't confuse the program's exit status with a
+ // process' termination status (but in the case
+ // when exit is used, the exit status becomes the
+ // process' termination status).
+ //
+    exit(42);
+
+}
